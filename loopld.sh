5f2637189875eda23bf9f13601934272b527f85d
Tingjun Yang <tjyang@fnal.gov>
2024-03-05 09:49:10 -0600

    Add plotting scripts to the loop.

--- loopld.sh
+++ loopld.sh
@@ -3,6 +3,8 @@ while true
 do
     echo "Run the top_ld.py"
     python ./top_ld.py #uncomment it to perform top_ld.py
+    python ./SBND_APA_ANA_v2.py
+    python ./SBND_RMS_vs_Time.py
     echo "Done, wait for 30 minutes for antoher data taking or terminate it."
     sleep 1800 #change to 30*60 for 30 minutes
 done
