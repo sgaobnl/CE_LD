3af519809b29d824cc52dadb8221b92b30227928
Tingjun Yang <tjyang@fnal.gov>
2024-03-06 22:00:41 -0600

    Improve conversion of image to base64.

--- email.sh
+++ email.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rmsfile=`ls -Art /scratch_local/SBND_Installation/data/commissioning/LD_result/LD*png | tail -n 1`
-export rms_base64=`cat $rmsfile | openssl base64`
+echo "RMS file: $rmsfile"
+export rms_base64=`base64 --wrap 0 $rmsfile`
 rmstimefile=/scratch_local/SBND_Installation/data/commissioning/LD_result/RMS_vs_Time.png
-export rmstime_base64=`cat $rmstimefile | openssl base64`
+echo "RMS vs Time: $rmstimefile"
+export rmstime_base64=`base64 --wrap 0 $rmstimefile`
 export filename=`basename $rmsfile`
 envsubst < email.eml | sendmail -t
