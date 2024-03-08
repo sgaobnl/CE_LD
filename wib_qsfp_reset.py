0bbf7508dcc398b1abf47a4311340295858a6dd4
sgaobnl <gao33.bnl@gmail.com>
2024-03-08 14:06:00 -0500

    add a script to reset QSFP

---
+++ wib_qsfp_reset.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+File Name: cls_femb_config.py
+Author: GSS
+Mail: gao.hillhill@gmail.com
+Description: 
+Created Time: 3/20/2019 4:50:34 PM
+Last modified: Fri Mar  8 14:03:33 2024
+"""
+
+#defaut setting for scientific caculation
+#import numpy
+#import scipy
+#from numpy import *
+import numpy as np
+#import scipy as sp
+#import pylab as pl
+
+import sys 
+import os
+import time
+from datetime import datetime
+from cls_config import CLS_CONFIG
+
+LD = CLS_CONFIG()
+LD.ldflg=True
+LD.WIB_IPs = [
+              "10.226.34.11",
+              "10.226.34.12",
+              "10.226.34.13",
+              "10.226.34.14",
+              "10.226.34.15",
+#              "10.226.34.16"
+              "10.226.34.16", 
+              "10.226.34.21",
+              "10.226.34.22",
+              "10.226.34.23",
+              "10.226.34.24",
+              "10.226.34.25",
+              "10.226.34.26", 
+              "10.226.34.31",
+              "10.226.34.32",
+              "10.226.34.33",
+              "10.226.34.34",
+              "10.226.34.35",
+              "10.226.34.36",
+              "10.226.34.41",
+              "10.226.34.42",
+              "10.226.34.43",
+              "10.226.34.44",
+              "10.226.34.45",
+              "10.226.34.46"
+              ]
+
+LD.UDP.MultiPort = True
+for wib_ip in LD.WIB_IPs:
+    LD.UDP.UDP_IP = wib_ip
+    LD.UDP.write_reg_wib(0x14, 0xCF)
+    val = LD.UDP.read_reg_wib(0x14)
+    print (wib_ip, val)
+time.sleep(1)
+for wib_ip in LD.WIB_IPs:
+    LD.UDP.UDP_IP = wib_ip
+    LD.UDP.write_reg_wib(0x14, 0x100)
+    val = LD.UDP.read_reg_wib(0x14)
+    print (wib_ip, val)
+time.sleep(1)
+for wib_ip in LD.WIB_IPs:
+    LD.UDP.UDP_IP = wib_ip
+    LD.UDP.write_reg_wib(0x14, 0x000)
+    val = LD.UDP.read_reg_wib(0x14)
+    print (wib_ip, val)
+time.sleep(1)
+
+print ("Done")
+
