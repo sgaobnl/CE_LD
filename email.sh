9fc4ec577640d89ae5055cb21147df0bd1798bcf
sbnd <sbnd@fnal.gov>
2024-03-12 16:06:47 -0500

    Update email scripts.

--- email.sh
+++ email.sh
@@ -6,19 +6,19 @@ lastfile=`basename $lastfilepath`
 filepattern=`echo $lastfile | cut -d_ -f1-8`
 #echo $filepattern
 
-NUM_OF_FILE=0
+rmstimefile=/scratch_local/SBND_Installation/data/commissioning/LD_result/RMS_vs_Time.png
+export rmstime_base64=`base64 --wrap 0 $rmstimefile`
+echo $rmstimefile
+
+NUM_OF_FILE=1
 while read i
 do
     echo "$i"
-    NUM_OF_FILE=$(( $NUM_OF_FILE+1 ))
     export file${NUM_OF_FILE}=`base64 --wrap 0 $i`
-    export filename${NUM_OF_FILE}=$i
-done < <(find /scratch_local/SBND_Installation/data/commissioning/LD_result/ -name "$filepattern*" -type f)
+    export filename${NUM_OF_FILE}=`basename $i`
+    NUM_OF_FILE=$(( $NUM_OF_FILE+1 ))
+done < <(find /scratch_local/SBND_Installation/data/commissioning/LD_result/ -name "$filepattern*" -type f | sort -r)
 
 echo "Sending ${NUM_OF_FILE} images"
 
-rmstimefile=/scratch_local/SBND_Installation/data/commissioning/LD_result/RMS_vs_Time.png
-#echo "RMS vs Time: $rmstimefile"
-export rmstime_base64=`base64 --wrap 0 $rmstimefile`
-#export filename=`basename $rmsfile`
 envsubst < email.eml | sendmail -t
