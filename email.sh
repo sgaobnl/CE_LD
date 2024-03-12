b27f132c23475861674c0cb730475169afe9374a
sbnd <sbnd@fnal.gov>
2024-03-12 13:32:57 -0500

    Configure the script to email 20 images to slack.

--- email.sh
+++ email.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-rmsfile=`ls -Art /scratch_local/SBND_Installation/data/commissioning/LD_result/LD*png | tail -n 1`
-echo "RMS file: $rmsfile"
-export rms_base64=`base64 --wrap 0 $rmsfile`
+lastfilepath=`find /scratch_local/SBND_Installation/data/commissioning/LD_result/ -name 'LD*.png' -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1`
+lastfile=`basename $lastfilepath`
+#echo $lastfile
+filepattern=`echo $lastfile | cut -d_ -f1-8`
+#echo $filepattern
+
+NUM_OF_FILE=0
+while read i
+do
+    echo "$i"
+    NUM_OF_FILE=$(( $NUM_OF_FILE+1 ))
+    export file${NUM_OF_FILE}=`base64 --wrap 0 $i`
+    export filename${NUM_OF_FILE}=$i
+done < <(find /scratch_local/SBND_Installation/data/commissioning/LD_result/ -name "$filepattern*" -type f)
+
+echo "Sending ${NUM_OF_FILE} images"
+
 rmstimefile=/scratch_local/SBND_Installation/data/commissioning/LD_result/RMS_vs_Time.png
-echo "RMS vs Time: $rmstimefile"
+#echo "RMS vs Time: $rmstimefile"
 export rmstime_base64=`base64 --wrap 0 $rmstimefile`
-export filename=`basename $rmsfile`
+#export filename=`basename $rmsfile`
 envsubst < email.eml | sendmail -t
