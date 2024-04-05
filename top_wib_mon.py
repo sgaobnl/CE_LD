fbc700b59238d67357c3384a1cefaac06ead261d
sgaobnl <gao33.bnl@gmail.com>
2024-04-05 09:06:21 -0400

    submit

---
+++ top_wib_mon.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+File Name: LD_femb_config.py
+Author: GSS
+Mail: gao.hillhill@gmail.com
+Description: 
+Created Time: 3/20/2019 4:50:34 PM
+Last modified: Fri Apr  5 01:27:15 2024
+"""
+
+#defaut setting for scientific caculation
+#import numpy
+#import scipy
+#from numpy import *
+import numpy as np
+#import scipy as sp
+#import pylab as pl
+
+import sys 
+import os
+import string
+import time
+from datetime import datetime
+import struct
+import codecs
+from cls_config import CLS_CONFIG
+from raw_convertor import RAW_CONV
+import pickle
+from shutil import copyfile
+#from setdatadir import savedir
+
+LD = CLS_CONFIG()
+LD.val=200
+
+
+LD.ldflg=True
+LD.UDP.MultiPort = True
+#LD.UDP.MultiPort = False
+LD.WIB_IPs = [
+        #              "10.226.34.11",
+                      "10.226.34.12",
+        #              "10.226.34.13",
+        #              "10.226.34.14",
+        #              "10.226.34.15",
+        #              "10.226.34.16", 
+        #              "10.226.34.21",
+        #              "10.226.34.22",
+        #              "10.226.34.23",
+        #              "10.226.34.24",
+        #              "10.226.34.25",
+        #              "10.226.34.26", 
+        #              "10.226.34.31",
+        #              "10.226.34.32",
+        #              "10.226.34.33",
+        #              "10.226.34.34",
+        #              "10.226.34.35",
+        #              "10.226.34.36",
+        #              "10.226.34.41",
+        #              "10.226.34.42",
+        #              "10.226.34.43",
+        #              "10.226.34.44",
+        #              "10.226.34.45",
+        #      "10.226.34.46"
+              ]
+#LD.WIB_IPs = ["192.168.121.1"]
+for wib_ip in LD.WIB_IPs:
+    print (wib_ip)
+    if ".16" in wib_ip:
+        LD.act_fembs[wib_ip] = [True, True, False, False]
+    elif ".26" in wib_ip:
+        LD.act_fembs[wib_ip] = [True, True, False, False]
+    elif ".36" in wib_ip:
+        LD.act_fembs[wib_ip] = [True, True, False, False]
+    elif ".46" in wib_ip:
+        LD.act_fembs[wib_ip] = [True, True, False, False]
+    else:
+        LD.act_fembs[wib_ip] = [True, True, True, True]
+
+#LD.act_fembs[wib_ip] = [True, False, False, False]
+savedir ="""/scratch_local/SBND_Installation/data/commissioning/mon_run/""" 
+print (savedir)
+#savedir ="""D:/full_run/""" 
+
+def Create_Folder():
+    runtime =  datetime.now().strftime('%Y_%m_%d_%H_%M_%S') 
+    print ("Test starts ", runtime)
+    LD.savedir = savedir + "/LD_" + runtime + "/"
+    if (os.path.exists(LD.savedir)):
+        pass
+    else:
+        try:
+            os.makedirs(LD.savedir)
+        except OSError:
+            print ("Error to create folder %s"%LD.savedir)
+            sys.exit()
+    print("Save data in "+LD.savedir)
+
+
+recfn = savedir + "record.txt"
+
+if True:
+    textnote = "{}:".format(datetime.now())
+    textnote += "Analog Monitoring \n"
+    with open(recfn, "a+") as rfp:
+        rfp.write(textnote)
+    print (textnote)
+    Create_Folder()
+    #cfglog = LD.CE_MON_CFG(pls_cs=0, dac_sel=0, mon_cs=1, sts=0, sg0=0, sg1=1, st0 =1, st1=1, snc=1, smnchn=0)
+    #cfglog = LD.CE_MON_CFG(pls_cs=0, dac_sel=0, mon_cs=1, sts=0, sg0=0, sg1=1, st0 =1, st1=1, snc=1, smnchn=0)
+    cfglog = LD.CE_MON_CFG(pls_cs=1, dac_sel=1, mon_cs=1, asicdac_en=1, sts=1, sg0=0, sg1=1, st0 =1, st1=1, snc=1,swdac1=0, swdac2=1, dac=0x10, smnchn=0)
+
+
+print ("Done")
+
