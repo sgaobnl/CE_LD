a3837cea0f0c0e246bd7e5cbda639cfb9f69994d
sbnd <sbnd@fnal.gov>
2024-04-28 08:55:36 -0500

    Add loopchecksum.sh.

---
+++ loopchecksum.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+while true
+do
+    echo "Run the top_link_stats.py"
+    python ./top_link_stats.py
+    #echo "Done, wait for 15 seconds"
+    echo "Done, wait for 30 minutes"
+    sleep 1800
+done
