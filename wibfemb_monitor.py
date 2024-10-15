bec4438a653ba58ede0a52c509985ed04be29a17
sbnd <sbnd@fnal.gov>
2024-10-15 16:01:53 -0500

    Running top_ld.py in the new script.

--- wibfemb_monitor.py
+++ wibfemb_monitor.py
@@ -8,12 +8,20 @@ nap_time = 900  # in second
 def main():
     try:
         while True:
+            print("Running top_ld.py")
+            subprocess.run(["python", "top_ld.py"])
+            subprocess.run(["python", "SBND_APA_ANA_v2.py"])
+            subprocess.run(["python", "SBND_RMS_vs_Time.py"])
+            subprocess.run(["python", "myemail.py"])
+            subprocess.run(["python", "myemail2.py"])
+            print("Sending femb status to EPICS")
             for crate in range(1, 5):
                 for slot in range(1, 7):
                     subprocess.run(["python", "femb_status_check_n_epics.py", str(crate), str(slot)])
-                    
+
             print(f"Done, waiting for {nap_time} seconds")
             time.sleep(nap_time)
+
     except KeyboardInterrupt:
         print("\nScript interrupted by user. Resetting EPICS PVs to -1 ...")
         for crate in range(1, 5):
