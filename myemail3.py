e64e6f87b64c2080f9fffbea888474962e0c648f
sbnd <sbnd@fnal.gov>
2024-12-13 09:41:17 -0600

    Add a script to send email if disk is at least 95% full.

---
+++ myemail3.py
@@ -0,0 +1,42 @@
+# check disk usage and send email if 95% full.
+
+import os
+import glob
+import psutil
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+# Define these once; use them twice!
+strFrom = 'sbndwib'
+#recipients = 'tjyang@fnal.gov'
+#recipients = 'aaaaf37do5cye5svsdg3jj2rkm@shortbaseline.slack.com'
+recipients = 'sbnd_cold_electronics-aaaamjjujebziez3xtqixfexxy@shortbaseline.slack.com, tjyang@fnal.gov, sgao@bnl.gov'
+
+# Define the partition to check 
+partition = '/scratch_local' 
+
+# Define the threshold (e.g., 95% full) 
+threshold = 95
+
+# Check disk usage
+usage = psutil.disk_usage(partition)
+percent_used = usage.percent
+
+# Send email if the partition is full
+if percent_used > threshold:
+    # Create the email content
+    message = MIMEMultipart()
+    message['From'] = strFrom
+    message['To'] = recipients
+    message['Subject'] = "Disk full"
+
+    body = f'The partition {partition} is {percent_used}% full.'
+    print(body)
+    print("Sending email")
+    message.attach(MIMEText(body, 'plain'))
+
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(strFrom, recipients.split(','), message.as_string())
+    smtp.quit()
--- wibfemb_monitor.py
+++ wibfemb_monitor.py
@@ -21,6 +21,7 @@ def main():
                 subprocess.run(["python", "SBND_RMS_vs_Time.py"])
                 subprocess.run(["python", "myemail.py"])
                 subprocess.run(["python", "myemail2.py"])
+                subprocess.run(["python", "myemail3.py"])
                 iskip = nskip
             else:
                 print(f"Run top_ld.py after {iskip*nap_time/60} minutes")
